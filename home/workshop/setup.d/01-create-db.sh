c75e4a484825f7e7f041f17a7a1d706340687820
tomsfrt <65518524+tomsfrt@users.noreply.github.com>
2021-01-31 17:54:40 -0500

    Update 01-create-db.sh

--- home/workshop/setup.d/01-create-db.sh
+++ home/workshop/setup.d/01-create-db.sh
@@ -4,4 +4,5 @@ set -x
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
+sleep 1000
 helm upgrade -i petclinic-db bitnami/mysql --version 6.14.11 -f <(cat petclinic-db-values.yaml | envsubst)
