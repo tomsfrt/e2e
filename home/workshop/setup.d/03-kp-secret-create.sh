04452fa457438262d2c677544c41ed84a14b68a0
Thomas Seufert <tseufert@gmail.com>
2021-02-02 22:38:18 -0500

    use external harbor, quick fix

--- home/workshop/setup.d/03-kp-secret-create.sh
+++ home/workshop/setup.d/03-kp-secret-create.sh
@@ -2,4 +2,4 @@
 
 set -x
 
-REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create harbor-creds --registry harbor.$INGRESS_DOMAIN --registry-user admin
+REGISTRY_PASSWORD=$HARBOR_PASSWORD kp secret create harbor-creds --registry harbor.e2e.tsfrt.info --registry-user admin
